b3eb14dfd738c4751b3f80b9644fc9cd792ee466
carsonp45 <carsonp3@nycstudents.net>
2026-03-23 10:17:08 -0400

    e

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 import json
 pokedex = open("./pokedex.json", encoding="utf8")
 data = json.load(pokedex)
-""" print(data[]) """
+""" print(data[0]) """
+
+language = input("Would you like to set your pokedex to English, Japanese, Chinese, or French")
 for index, item in enumerate(data):
-    print(index, ":", item["name"])
-language = (input("English, Japanese, Chinese, or French"))
-if language == "French":
-    print(f"{index}: {item["name"]["French"]}")
+    if language == "french":
+        print(f"{index}: {item["name"]["french"]}")
+    if language == "japanese":
+        print(f"{index}: {item["name"]["japanese"]}")
+    if language == "english":
+        print(f"{index}: {item["name"]["english"]}")
+    if language == "chinese":
+        print(f"{index}: {item["name"]["chinese"]}")
