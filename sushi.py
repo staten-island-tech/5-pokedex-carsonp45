b37ee56067351720898f9210555b85b40bedfd53
carsonp45 <carsonp3@nycstudents.net>
2026-03-31 10:16:47 -0400

    l

--- sushi.py
+++ sushi.py
@@ -12,13 +12,18 @@ sushi_orders = [
 ]
 
 def receipt(orders):
-    the_receipt = {"name": "California Roll", "price": 8}
-    for i in orders:
-       if i["name"] in the_receipt:
-         continue
-    else:
-           the_receipt[i["name"]] = {
-               "price": i["price"],
-               "qty": 3
-           }
-    print(the_receipt)
+    the_receipt = {}
+    for sushi in orders:
+        if sushi["name"] in the_receipt:
+         the_receipt[sushi['name']]['qty'] +=1
+        else:
+          the_receipt[sushi['name']] = {
+             'price': sushi['price'],
+             'qty': 1
+          }
+    for sushi, value in the_receipt.items():
+        price = value['price'] * value['qty']
+        print(sushi, value['qty'], price)
+
+
+receipt(sushi_orders)
