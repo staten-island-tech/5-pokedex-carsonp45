dff0830c90d7bbc0a1fbcce37d62b43964dbb8b3
carsonp45 <carsonp3@nycstudents.net>
2026-04-15 10:16:39 -0400

    67

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 import json
 pokedex = open("./pokedex.json", encoding="utf8")
 data = json.load(pokedex)
-""" print(data[0]) """ 
-
-""" language = input("Would you like to set your pokedex to english, japanese, chinese, or french>")
+ 
+language = input("Would you like to set your pokedex to english, japanese, chinese, or french>")
 for index, item in enumerate(data):
     if language == "french":
         print(f"{index}: {item["name"]["french"]}")
@@ -12,16 +11,21 @@ for index, item in enumerate(data):
     if language == "english":
         print(f"{index}: {item["name"]["english"]}")
     if language == "chinese":
-        print(f"{index}: {item["name"]["chinese"]}")" """
-type = input("What type(s) would you like?") 
+        print(f"{index}: {item["name"]["chinese"]}") 
+type = input("What type(s) would you like?")  
 for index, item in enumerate(data):
-    if type == "fire":
-        print(f"{index}: {item["name"]["fire"]}")
-    elif type == "water":
-        print(f"{index}: {item["name"]["type"]["water"]}")
-    elif type == "grass":
-        print(f"{index}: {item["name"]["type"]["grass"]}")
-    elif type == "flying":
-        print(f"{index}: {item["name"]["type"]["flying"]}")
-    elif type == "rock":
-        print(f"{index}: {item["name"]["type"]["rock"]}")   
+      if type in item['type']:
+        print(f"{index}: {item['name']['english']}")
+"""   if type == "Fire":
+        print(f"{index}: {item["type"]["Fire"]}")
+    elif type == "Water":
+        print(f"{index}: {item["name"]["type"]["Water"]}")
+    elif type == "Grass":
+        print(f"{index}: {item["name"]["type"]["Grass"]}")
+    elif type == "Flying":
+        print(f"{index}: {item["name"]["type"]["Flying"]}")
+    elif type == "Rock":
+        print(f"{index}: {item["name"]["type"]["Rock"]}")   
+    elif type == "Fairy":
+        print(f"{index}: {item["name"]["type"]["Fairy"]}") 
+ """
